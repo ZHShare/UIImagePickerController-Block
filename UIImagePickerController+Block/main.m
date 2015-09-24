12c61da839674bb5e58cf9726b2af81597d9c153
ZHShare <VIPHoa@163.com>
2015-09-24 22:35:24 +0800

    Initial Commit

---
+++ UIImagePickerController+Block/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIImagePickerController+Block
+//
+//  Created by Apple on 15/9/24.
+//  Copyright © 2015年 Zhipin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
